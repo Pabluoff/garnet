1ac07a021197fcdc5e49ab998228df7b142d68e1
Paluoff <gabriel999pablo@gmail.com>
2024-05-19 20:35:35 -0300

    .

--- _site/js/settings.js
+++ _site/js/settings.js
@@ -1,12 +1,11 @@
-function carregarNomeSalvo() {
-    const nomeSalvo = localStorage.getItem("nome");
-
-    if (nomeSalvo) {
-        const userSettingsElement = document.getElementById("user-settings");
-        userSettingsElement.textContent = nomeSalvo;
+document.addEventListener("DOMContentLoaded", function() {
+    // Recuperar o nome salvo no localStorage
+    const userName = localStorage.getItem("nome");
+    if (userName) {
+        document.getElementById("user-settings").textContent = userName;
+        
+        // Definir a inicial do nome no perfil
+        const profileInitial = document.getElementById("profile-initial");
+        profileInitial.textContent = userName.charAt(0).toUpperCase();
     }
-}
-
-window.onload = function () {
-    carregarNomeSalvo();
-};
+});
