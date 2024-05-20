6fa5b9fd231b014aa379de538ac6dd41724ff26d
Paluoff <gabriel999pablo@gmail.com>
2024-05-19 22:51:29 -0300

    ,

--- js/settings.js
+++ js/settings.js
@@ -1,11 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Recuperar o nome salvo no localStorage
     const userName = localStorage.getItem("nome");
     if (userName) {
         document.getElementById("user-settings").textContent = userName;
-        
+
         // Definir a inicial do nome no perfil
         const profileInitial = document.getElementById("profile-initial");
         profileInitial.textContent = userName.charAt(0).toUpperCase();
     }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("https://ipinfo.io/json")
+        .then((response) => response.json())
+        .then((data) => {
+            let providerName = data.org.replace(/\d+/g, "").replace(/AS/g, "");
+            document.getElementById("providerInfo").innerText = providerName.trim();
+        });
 });
