36281de30e1e0a4b08e9ec33acfe60a78b20556a
Paluoff <gabriel999pablo@gmail.com>
2024-06-08 22:42:11 -0300

    ,

--- _site/sw.js
+++ _site/sw.js
@@ -7,19 +7,3 @@ self.addEventListener ('activate', evt => {
     console.log ('Service Worker ativado');
 });
 
-// service-worker.js
-
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('push', (event) => {
-    const options = {
-        body: 'Esta é uma notificação enviada automaticamente.',
-        icon: '/img/logo.jpg', // Caminho para um ícone opcional
-        badge: '/img/logo.jpg' // Caminho para um ícone opcional
-    };
-    event.waitUntil(
-        self.registration.showNotification('Notificação Automática', options)
-    );
-});
