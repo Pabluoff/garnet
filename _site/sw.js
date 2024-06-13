08891b6132390b12e3fae319b83e1d64012a6adc
Paluoff <gabriel999pablo@gmail.com>
2024-06-13 20:52:55 -0300

    .

--- _site/sw.js
+++ _site/sw.js
@@ -1,15 +1,1 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker instalado');
-    self.skipWaiting();
-});
-
-self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.json() : { title: 'Sem título', body: 'Sem corpo' };
-    const options = {
-        body: data.body,
-        icon: '/img/logo.jpg', 
-    };
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
-});
+importScripts("https://cdn.pushalert.co/sw-71333.js");
