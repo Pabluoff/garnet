ba4ccbea451561f60e64c3501cda7d8d25ad6c54
Paluoff <gabriel999pablo@gmail.com>
2024-06-13 19:47:25 -0300

    ,

--- _site/js/script.js
+++ _site/js/script.js
@@ -213,3 +213,59 @@ function verificarConexaoInternet() {
 
   updateStatus(navigator.onLine);
 }
+
+function solicitarPermissaoNotificacao() {
+  if (Notification.permission !== 'granted') {
+    Notification.requestPermission().then(permission => {
+      if (permission === 'granted') {
+        registrarServiceWorker();
+        enviarNotificacaoDiaria();
+      }
+    });
+  } else {
+    registrarServiceWorker();
+    enviarNotificacaoDiaria();
+  }
+}
+
+function registrarServiceWorker() {
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    navigator.serviceWorker.register('/sw.js')
+      .then(function (registration) {
+        console.log('Service Worker registrado com sucesso:', registration);
+      })
+      .catch(function (error) {
+        console.error('Falha ao registrar o Service Worker:', error);
+      });
+  } else {
+    console.warn('Push messaging não é suportado');
+  }
+}
+
+function enviarNotificacao(titulo, mensagem) {
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    navigator.serviceWorker.ready.then(function (registration) {
+      registration.showNotification(titulo, {
+        body: mensagem,
+        icon: '/img/logo.jpg', // Caminho para o ícone da notificação
+      });
+    });
+  }
+}
+
+function enviarNotificacaoDiaria() {
+  const agora = new Date();
+  const horasRestantes = 24 - agora.getHours();
+  const minutosRestantes = 60 - agora.getMinutes();
+  const segundosRestantes = 60 - agora.getSeconds();
+  const tempoRestante = (horasRestantes * 3600 + minutosRestantes * 60 + segundosRestantes) * 1000;
+
+  setTimeout(function () {
+    enviarNotificacao('Lembrete Diário', 'Esta é sua notificação diária!');
+    setInterval(function () {
+      enviarNotificacao('Lembrete Diário', 'Esta é sua notificação diária!');
+    }, 24 * 60 * 60 * 1000);
+  }, tempoRestante);
+}
+
+document.addEventListener('DOMContentLoaded', solicitarPermissaoNotificacao);
--- _site/sw.js
+++ _site/sw.js
@@ -1,9 +1,17 @@
-//install
-self.addEventListener ('install', evt => {
-    console.log ('Service Worker instalado');
+// service-worker.js
+
+self.addEventListener('install', function(event) {
+    console.log('Service Worker instalado');
+    self.skipWaiting();
 });
 
-self.addEventListener ('activate', evt => {
-    console.log ('Service Worker ativado');
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: '/img/logo.jpg', 
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
 });
-
