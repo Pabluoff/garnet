5684f12f320d5abc70ffbf3204b985b9efd48278
Pabluoff <gabriel999pablo@gmail.com>
2024-12-07 01:47:37 -0300

    .

--- js/script.js
+++ js/script.js
@@ -301,9 +301,3 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-document.body.addEventListener('mousedown', event => event.preventDefault());
-document.body.addEventListener('mousemove', event => event.preventDefault());
-document.body.addEventListener('mouseup', event => event.preventDefault());
-document.body.addEventListener('touchstart', event => event.preventDefault());
-document.body.addEventListener('touchmove', event => event.preventDefault());
-document.body.addEventListener('touchend', event => event.preventDefault());
